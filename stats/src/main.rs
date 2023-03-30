48904b1ccfd006b17a9067a3e07dd2ef8772881a
Martynas <martynas.subonis@crowdhouse.ch>
2023-03-30 11:24:34 +0200

    further refactor stats

--- stats/src/main.rs
+++ stats/src/main.rs
@@ -14,8 +14,28 @@ const TRADE_COUNT_PLOT_FILE_NAME: &str = "generated/trade_count.png";
 
 pub fn main() {
     let data = load_data();
-    draw_networth_histogram(&data);
-    draw_trade_counts_histogram(&data);
+    draw_histogram(
+        &data,
+        NETWORTH_PLOT_FILE_NAME,
+        (0, 40000),
+        (0, 800),
+        "Networth Distribution",
+        "Networth",
+        "Count",
+        12,
+        |x| round_to_precision(x.net_worth as u32, 1000),
+    );
+    draw_histogram(
+        &data,
+        TRADE_COUNT_PLOT_FILE_NAME,
+        (0, 250),
+        (0, 700),
+        "Trade Counts Distribution",
+        "Trade Counts",
+        "Count",
+        5,
+        |x| x.trade_count as u32,
+    );
     stdout_stats(&data);
 }
 
@@ -25,25 +45,40 @@ fn load_data() -> HashMap<String, Vec<AgentRunStats>> {
     return serde_json::from_reader(reader).unwrap();
 }
 
-fn draw_networth_histogram(data: &HashMap<String, Vec<AgentRunStats>>) -> () {
-    let draw_area = BitMapBackend::new(NETWORTH_PLOT_FILE_NAME, (1280, 960)).into_drawing_area();
+fn draw_histogram<F>(
+    data: &HashMap<String, Vec<AgentRunStats>>,
+    plot_file_name: &str,
+    x_axis_range: (u32, u32),
+    y_axis_range: (u32, u32),
+    caption: &str,
+    x_axis_label: &str,
+    y_axis_label: &str,
+    bar_width: u32,
+    value_mapper: F,
+) where
+    F: Fn(&AgentRunStats) -> u32,
+{
+    let draw_area = BitMapBackend::new(plot_file_name, (1280, 960)).into_drawing_area();
     draw_area.fill(&WHITE).unwrap();
 
     let mut chart = ChartBuilder::on(&draw_area)
         .x_label_area_size(60)
         .y_label_area_size(60)
         .margin(20)
-        .caption("Networth Distribution", ("sans-serif", 50.0))
-        .build_cartesian_2d(0u32..40000u32, 0u32..800u32)
+        .caption(caption, ("sans-serif", 50.0))
+        .build_cartesian_2d(
+            x_axis_range.0..x_axis_range.1,
+            y_axis_range.0..y_axis_range.1,
+        )
         .unwrap();
 
     chart
         .configure_mesh()
         .disable_x_mesh()
         .bold_line_style(&BLACK.mix(0.3))
-        .y_desc("Count")
+        .y_desc(y_axis_label)
         .y_label_style(("sans-serif", 20))
-        .x_desc("Networth")
+        .x_desc(x_axis_label)
         .x_label_style(("sans-serif", 20))
         .axis_desc_style(("sans-serif", 30))
         .draw()
@@ -53,71 +88,9 @@ fn draw_networth_histogram(data: &HashMap<String, Vec<AgentRunStats>>) -> () {
         chart
             .draw_series(
                 Histogram::vertical(&chart)
-                    .margin(12)
+                    .margin(bar_width)
                     .style(get_color(key).filled())
-                    .data(
-                        value
-                            .iter()
-                            .map(|x| (round_to_precision(x.net_worth as u32, 1000), 1)),
-                    ),
-            )
-            .unwrap()
-            .label(key)
-            .legend(move |(x, y)| {
-                PathElement::new(
-                    vec![(x, y), (x + 20, y)],
-                    ShapeStyle {
-                        color: get_color(key),
-                        filled: false,
-                        stroke_width: 4,
-                    },
-                )
-            });
-    }
-
-    chart
-        .configure_series_labels()
-        .border_style(&BLACK)
-        .legend_area_size(30)
-        .background_style(&WHITE.mix(0.8))
-        .label_font(("sans-serif", 20))
-        .draw()
-        .unwrap();
-
-    draw_area.present().unwrap();
-}
-
-fn draw_trade_counts_histogram(data: &HashMap<String, Vec<AgentRunStats>>) -> () {
-    let draw_area = BitMapBackend::new(TRADE_COUNT_PLOT_FILE_NAME, (1280, 960)).into_drawing_area();
-    draw_area.fill(&WHITE).unwrap();
-
-    let mut chart = ChartBuilder::on(&draw_area)
-        .x_label_area_size(60)
-        .y_label_area_size(60)
-        .margin(20)
-        .caption("Trade Counts Distribution", ("sans-serif", 50.0))
-        .build_cartesian_2d(0u32..250u32, 0u32..700u32)
-        .unwrap();
-
-    chart
-        .configure_mesh()
-        .disable_x_mesh()
-        .bold_line_style(&BLACK.mix(0.3))
-        .y_desc("Count")
-        .y_label_style(("sans-serif", 20))
-        .x_desc("Trade Counts")
-        .x_label_style(("sans-serif", 20))
-        .axis_desc_style(("sans-serif", 30))
-        .draw()
-        .unwrap();
-
-    for (key, value) in data {
-        chart
-            .draw_series(
-                Histogram::vertical(&chart)
-                    .margin(5)
-                    .style(get_color(key).filled())
-                    .data(value.iter().map(|x| (x.trade_count as u32, 1))),
+                    .data(value.iter().map(|x| (value_mapper(x), 1))),
             )
             .unwrap()
             .label(key)
