354ff7470d4e7718aa2c1fb1a8708b8810fad4b0
Martynas Subonis <martynas.subonis@gmail.com>
2023-03-31 12:25:41 +0200

    chore: standardize namings (#3)

--- mc/src/util/windows.rs
+++ mc/src/util/windows.rs
@@ -4,7 +4,7 @@ const SMALL_WINDOW: usize = SCALE_FACTOR;
 const MEDIUM_WINDOW: usize = SMALL_WINDOW * SCALE_FACTOR;
 const LARGE_WINDOW: usize = MEDIUM_WINDOW * SCALE_FACTOR;
 
-pub fn get_available_windows(n: usize) -> Vec<usize> {
+pub fn create_available_windows(n: usize) -> Vec<usize> {
     vec![LARGE_WINDOW, MEDIUM_WINDOW, SMALL_WINDOW]
         .into_iter()
         .filter(|x| x <= &n)
@@ -16,14 +16,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn get_available_windows_returns_filters_windows_that_are_to_long() {
-        let available_windows = get_available_windows(1296);
+    fn create_available_windows_returns_filters_windows_that_are_to_long() {
+        let available_windows = create_available_windows(1296);
         assert_eq!(available_windows, vec![MEDIUM_WINDOW, SMALL_WINDOW]);
     }
 
     #[test]
-    fn get_available_windows_returns_empty_vec_when_no_windows_available() {
-        let available_windows = get_available_windows(1);
+    fn create_available_windows_returns_empty_vec_when_no_windows_available() {
+        let available_windows = create_available_windows(1);
         assert_eq!(available_windows, Vec::<usize>::new());
     }
 }
